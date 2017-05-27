be39dc1243c039d3874b8e4d4554617968811301
Don <don@erealmsoft.com>
2017-05-27 10:28:35 +0800

    Format code;

--- components/cq-ng-ui.js
+++ components/cq-ng-ui.js
@@ -1,23 +1,23 @@
 CqNgUi.$inject = ['$element', '$scope', '$rootScope'];
 
-function CqNgUi($element,$scope, $rootScope){
-	var ctrl=this;
-	ctrl.drawingEnabled=false;
+function CqNgUi($element, $scope, $rootScope) {
+	var ctrl = this;
+	ctrl.drawingEnabled = false;
 
-	ctrl.$postLink=function(){
-		$scope.example3=$element[0].baseURI.endsWith('3.html'); //part of the UI is only for example-3
-		$rootScope.$on('updateThemeList', function(event, themeObj, themeName){
-			if(themeName){ // we aren't going to allow unnamed themes to be created
-				var duplicate=false;
+	ctrl.$postLink = function () {
+		$scope.example3 = $element[0].baseURI.endsWith('3.html'); //part of the UI is only for example-3
+		$rootScope.$on('updateThemeList', function (event, themeObj, themeName) {
+			if (themeName) { // we aren't going to allow unnamed themes to be created
+				var duplicate = false;
 				var newTheme = {"name": themeName, "settings": themeObj};
-				for(var i=0; i<ctrl.themes.length; i++){
-					if(ctrl.themes[i].name==themeName) {
+				for (var i = 0; i < ctrl.themes.length; i++) {
+					if (ctrl.themes[i].name === themeName) {
 						ctrl.themes[i].settings = newTheme.settings;
-						duplicate=true;
+						duplicate = true;
 					}
 				}
-				if(!duplicate) { // if it's duplicate we have updated that existing theme
-					ctrl.themes.splice((ctrl.themes.length-1), 0, newTheme);
+				if (!duplicate) { // if it's duplicate we have updated that existing theme
+					ctrl.themes.splice((ctrl.themes.length - 1), 0, newTheme);
 				}
 				$rootScope.$broadcast('updateTheme', newTheme);
 			}
@@ -25,20 +25,20 @@ function CqNgUi($element,$scope, $rootScope){
 		});
 	};
 
-	ctrl.addStudy=function(study){
-		$rootScope.$broadcast('addStudy',study, ctrl.cqNgChart.ciq);
+	ctrl.addStudy = function (study) {
+		$rootScope.$broadcast('addStudy', study, ctrl.cqNgChart.ciq);
 	};
 
-	ctrl.launchTimezoneDialog=function(){
+	ctrl.launchTimezoneDialog = function () {
 		$rootScope.$broadcast('showTimezoneDialog', ctrl.cqNgChart.ciq);
 	};
 
-	ctrl.toggleDrawingToolbar=function(){
-		var callback=function(toolbarOn){
-			ctrl.drawingEnabled=toolbarOn;
+	ctrl.toggleDrawingToolbar = function () {
+		var callback = function (toolbarOn) {
+			ctrl.drawingEnabled = toolbarOn;
 			//resize the chart based on if the toolbar is now open or closed
 			var elem = document.getElementById("chartContainer");
-			if(toolbarOn)
+			if (toolbarOn)
 				elem.className += " toolbarOn";
 			else elem.classList.remove("toolbarOn");
 			ctrl.cqNgChart.ciq.draw();
@@ -46,33 +46,33 @@ function CqNgUi($element,$scope, $rootScope){
 		$rootScope.$broadcast('toggleDrawingToolbar', ctrl.cqNgChart.ciq, callback);
 	};
 
-	ctrl.handleThemeSelect=function(theme){
-		if(theme.name=="+ New Theme"){
+	ctrl.handleThemeSelect = function (theme) {
+		if (theme.name === "+ New Theme") {
 			$rootScope.$broadcast('showThemeDialog', ctrl.cqNgChart.ciq);
 		}
-		else{
+		else {
 			$rootScope.$broadcast('updateTheme', theme, ctrl.cqNgChart.ciq);
 		}
 	};
 
 	// Data for all the menus
-	ctrl.themes=[
+	ctrl.themes = [
 		{
 			"name": "Default",
 			"settings": { // the default theme settings
 				"chart": {
-					"Axis Text": { "color": "rgba(197,199,201,1)" },
-					"Background": { "color": "rgba(28,42,53,1)" },
-					"Grid Dividers": { "color": "rgba(37,55,70,1)" },
-					"Grid Lines": { "color": "rgba(33,50,63,1)" }
+					"Axis Text": {"color": "rgba(197,199,201,1)"},
+					"Background": {"color": "rgba(28,42,53,1)"},
+					"Grid Dividers": {"color": "rgba(37,55,70,1)"},
+					"Grid Lines": {"color": "rgba(33,50,63,1)"}
 				},
 				"chartTypes": {
 					"Candle/Bar": {
-						"down": { "border": "rgba(227,70,33,1)", "color": "rgba(184,44,12,1)", "wick": "rgba(0,0,0,1)" },
-						"up": { "border": "rgba(184,222,168,1)", "color": "rgba(140,193,118,1)", "wick": "rgba(0,0,0,1)" }
+						"down": {"border": "rgba(227,70,33,1)", "color": "rgba(184,44,12,1)", "wick": "rgba(0,0,0,1)"},
+						"up": {"border": "rgba(184,222,168,1)", "color": "rgba(140,193,118,1)", "wick": "rgba(0,0,0,1)"}
 					},
-					"Line": { "color": "rgba(0,0,0,1)" },
-					"Mountain": { "color": "rgba(102,202,196,0.498039)" }
+					"Line": {"color": "rgba(0,0,0,1)"},
+					"Mountain": {"color": "rgba(102,202,196,0.498039)"}
 				}
 			}
 		},
@@ -81,129 +81,129 @@ function CqNgUi($element,$scope, $rootScope){
 		}
 	];
 
-	ctrl.studies={
-		list:Object.keys(CIQ.Studies.studyLibrary),
-		selectedOption:''
+	ctrl.studies = {
+		list: Object.keys(CIQ.Studies.studyLibrary),
+		selectedOption: ''
 	};
 
-	ctrl.periodicity={
+	ctrl.periodicity = {
 		options: [
 			{
 				period: 1,
 				interval: 1,
-				label: '1 Min',
+				label: '1 Min'
 			},
 			{
 				period: 1,
 				interval: 3,
-				label: '3 Min',
+				label: '3 Min'
 			},
 			{
 				period: 1,
 				interval: 5,
-				label: '5 Min',
+				label: '5 Min'
 			},
 			{
 				period: 1,
 				interval: 10,
-				label: '10 Min',
+				label: '10 Min'
 			},
 			{
 				period: 3,
 				interval: 5,
-				label: '15 Min',
+				label: '15 Min'
 			},
 			{
 				period: 1,
 				interval: 30,
-				label: '30 Min',
+				label: '30 Min'
 			},
 			{
 				period: 2,
 				interval: 30,
-				label: '1 Hour',
+				label: '1 Hour'
 			},
 			{
 				period: 8,
 				interval: 30,
-				label: '4 Hour',
+				label: '4 Hour'
 			},
 			{
 				period: 1,
-				interval:'day',
-				label: '1 Day',
+				interval: 'day',
+				label: '1 Day'
 			},
 			{
 				period: 2,
-				interval:'day',
-				label: '2 Day',
+				interval: 'day',
+				label: '2 Day'
 			},
 			{
 				period: 3,
-				interval:'day',
-				label: '3 Day',
+				interval: 'day',
+				label: '3 Day'
 			},
 			{
 				period: 5,
-				interval:'day',
-				label: '5 Day',
+				interval: 'day',
+				label: '5 Day'
 			},
 			{
 				period: 10,
-				interval:'day',
-				label: '10 Day',
+				interval: 'day',
+				label: '10 Day'
 			},
 			{
 				period: 20,
-				interval:'day',
-				label: '20 Day',
+				interval: 'day',
+				label: '20 Day'
 			},
 			{
 				period: 1,
-				interval:'week',
-				label: '1 Wk',
+				interval: 'week',
+				label: '1 Wk'
 			},
 			{
 				period: 1,
-				interval:'month',
-				label: '1 Mon',
-			},
-		],
+				interval: 'month',
+				label: '1 Mon'
+			}
+		]
 	};
 
 	ctrl.chartTypes = {
 		types: [
 			{
 				type: 'bar',
-				label: 'Bar',
+				label: 'Bar'
 			},
 			{
 				type: 'candle',
-				label: 'Candle',
+				label: 'Candle'
 			},
 			{
 				type: 'colored_bar',
-				label: 'Colored bar',
+				label: 'Colored bar'
 			},
 			{
 				type: 'hollow_candle',
-				label: 'Hollow candle',
+				label: 'Hollow candle'
 			},
 			{
 				type: 'line',
-				label: 'Line',
+				label: 'Line'
 			},
 			{
 				type: 'mountain',
-				label: 'Mountain',
+				label: 'Mountain'
 			},
 			{
 				type: 'volume_candle',
-				label: 'Volume candle',
+				label: 'Volume candle'
 			},
 			{
 				type: 'heikinashi',
-				label: 'Heikin-Ashi',
+				label: 'Heikin-Ashi'
 			},
 			{
 				type: 'kagi',
@@ -213,7 +213,7 @@ function CqNgUi($element,$scope, $rootScope){
 					inputs: [
 						{
 							lookup: 'kagi',
-							label: 'Kagi',
+							label: 'Kagi'
 						}
 					]
 				}
@@ -275,12 +275,12 @@ function CqNgUi($element,$scope, $rootScope){
 				}
 			}
 		],
-		selectedOption: {type:'bar', label:'Bar'}
+		selectedOption: {type: 'bar', label: 'Bar'}
 	};
 }
 
 angular.module('cqNgApp').component('cqNgUi', {
-	controller:CqNgUi,
+	controller: CqNgUi,
 	templateUrl: 'templates/cq-ng-ui.html',
-	controllerAs:'cqNgUi',
+	controllerAs: 'cqNgUi'
 });
--- components/timezone-dialog.js
+++ components/timezone-dialog.js
@@ -2,46 +2,46 @@ TimezoneDialog.$inject = ['$scope'];
 
 function TimezoneDialog($scope) {
 	var ctrl = this;
-	ctrl.launchDialog=false;
-	ctrl.timezones=CIQ.timeZoneMap;
+	ctrl.launchDialog = false;
+	ctrl.timezones = CIQ.timeZoneMap;
 	//This array is needed so we can use orderBy
-	ctrl.arrOfTimezones = Object.keys(ctrl.timezones).map(function(key) {
+	ctrl.arrOfTimezones = Object.keys(ctrl.timezones).map(function (key) {
 		return ctrl.timezones[key];
 	});
-	ctrl.myZone=true; //default behavior
+	ctrl.myZone = true; //default behavior
 
-	ctrl.$postLink=function(){
-		$scope.$on('showTimezoneDialog', function(event, ciq){
-			ctrl.ciq=ciq;
-			ctrl.launchDialog=true;
+	ctrl.$postLink = function () {
+		$scope.$on('showTimezoneDialog', function (event, ciq) {
+			ctrl.ciq = ciq;
+			ctrl.launchDialog = true;
 		});
 	};
-	ctrl.closeMe=function(){
-		ctrl.launchDialog=false;
+	ctrl.closeMe = function () {
+		ctrl.launchDialog = false;
 	};
-	ctrl.setTimezone=function(zone){
+	ctrl.setTimezone = function (zone) {
 		ctrl.ciq.setTimeZone(ctrl.ciq.dataZone, zone);
-		if(ctrl.ciq.chart.symbol) ctrl.ciq.draw();
-		ctrl.myZone=false;
-		ctrl.launchDialog=false;
+		if (ctrl.ciq.chart.symbol) ctrl.ciq.draw();
+		ctrl.myZone = false;
+		ctrl.launchDialog = false;
 	};
-	ctrl.setMyTimezone=function(){
-		ctrl.ciq.defaultDisplayTimeZone=null;
-		for(var i=0;i<CIQ.ChartEngine.registeredContainers.length;i++){
-			var stx=CIQ.ChartEngine.registeredContainers[i].stx;
-			stx.displayZone=null;
-			ctrl.myZone=true;
+	ctrl.setMyTimezone = function () {
+		ctrl.ciq.defaultDisplayTimeZone = null;
+		for (var i = 0; i < CIQ.ChartEngine.registeredContainers.length; i++) {
+			var stx = CIQ.ChartEngine.registeredContainers[i].stx;
+			stx.displayZone = null;
+			ctrl.myZone = true;
 			stx.setTimeZone();
 
-			if(stx.displayInitialized) stx.draw();
+			if (stx.displayInitialized) stx.draw();
 		}
-		if(ctrl.ciq.chart.symbol) ctrl.ciq.draw();
+		if (ctrl.ciq.chart.symbol) ctrl.ciq.draw();
 		ctrl.closeMe();
 	};
 }
 
 angular.module('cqNgApp').component('timezoneDialog', {
-	controller:TimezoneDialog,
+	controller: TimezoneDialog,
 	templateUrl: 'templates/timezone-dialog.html',
-	controllerAs:'timezoneDialog'
+	controllerAs: 'timezoneDialog'
 });
